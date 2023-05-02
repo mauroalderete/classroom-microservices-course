ca909e1ec9c5035b5581bdd0e7820469a8596745
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-02 17:10:57 -0300

    feat: incorporate simple pagination extension

---
+++ KodotiCommerce/src/Services/Common/Service.Common.Mapping/DtoMapperExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Service.Common.Mapping
+{
+    public static class DtoMapperExtension
+    {
+        public static T MapTo<T>(this object value) where T : class
+        {
+            return JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(value));
+        }
+    }
+}
