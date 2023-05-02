6537d873e5d4708908c3d41af27c353d67ef5e0e
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-02 17:59:17 -0300

    feat: create command to send a new product to the context

---
+++ KodotiCommerce/src/Services/Catalog/Catalog.Service.EventHandlers/ProductCreateEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.Domain;
+using Catalog.Persistence.Database;
+using Catalog.Service.EventHandlers.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Catalog.Service.EventHandlers
+{
+    public class ProductCreateEventHandler : INotificationHandler<ProductCreateCommand>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductCreateEventHandler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(ProductCreateCommand notification, CancellationToken cancellationToken)
+        {
+            await _dbContext.AddAsync(
+                new Product
+                {
+                    Name = notification.Name,
+                    Description = notification.Description,
+                    Price = notification.Price
+                });
+
+            await _dbContext.SaveChangesAsync();
+
+        }
+    }
+}
