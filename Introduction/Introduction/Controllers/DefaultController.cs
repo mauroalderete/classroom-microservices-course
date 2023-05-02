be65f157ea6d89f39866766dc945b0469ff5141b
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-02 17:33:52 -0300

    feat: default controller and launch url at /

---
+++ Introduction/Introduction/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Introduction.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public string Index() {
+            return "Running...";
+        }
+    }
+}
