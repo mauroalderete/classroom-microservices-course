82ff2df8aa53f4728dca3a69f10b0349e39f690c
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-02 17:33:25 -0300

    feat: controller to handle products requests

---
+++ KodotiCommerce/src/Services/Catalog/Catalog.API/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Catalog.Service.Queries;
+using Catalog.Service.Queries.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Service.Common.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductQueryService _productQueryService;
+
+        public ProductController(ILogger<ProductController> logger, IProductQueryService productQuery)
+        {
+            _logger = logger;
+            _productQueryService = productQuery;
+        }
+
+        [HttpGet]
+        public async Task<DataCollection<ProductDto>> GetAll(
+            int page = 1,
+            int take = 10,
+            string ids = null)
+        {
+            IEnumerable<int> products = null;
+
+            if (!string.IsNullOrEmpty(ids))
+            {
+                products = ids.Split(',').Select(x => Convert.ToInt32(x));
+            }
+
+            return await _productQueryService.GetAllAsync(page, take, products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ProductDto> Get(int id)
+        {
+            return await _productQueryService.GetAsync(id);
+        }
+    }
+}
