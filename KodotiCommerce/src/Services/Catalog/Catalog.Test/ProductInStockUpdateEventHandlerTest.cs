1e4520ecf98ce2c485e5ab6443496d03d2971f37
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-03 13:28:41 -0300

    feat: test to update stock when it is subtracted and is not enough

--- KodotiCommerce/src/Services/Catalog/Catalog.Test/ProductInStockUpdateEventHandlerTest.cs
+++ KodotiCommerce/src/Services/Catalog/Catalog.Test/ProductInStockUpdateEventHandlerTest.cs
@@ -1,10 +1,12 @@
 using Castle.Core.Logging;
 using Catalog.Service.EventHandlers;
 using Catalog.Service.EventHandlers.Commands;
+using Catalog.Service.EventHandlers.Exceptions;
 using Catalog.Test.Config;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -49,6 +51,47 @@ namespace Catalog.Test
                     }
                 }
             }, new CancellationToken()).Wait();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ProductInStockUpdateCommandException))]
+        public void TryTosubstracStockWhenProductHasNotStock()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+
+            const int productStockId = 2;
+            const int productId = 2;
+
+            context.Stocks.Add(new Domain.ProductInStock()
+            {
+                ProductId = productId,
+                ProductInStockId = productStockId,
+                Stock = 1,
+            });
+
+            context.SaveChanges();
+
+            var handler = new ProductInStockUpdateStockEventHandler(context, GetLogger);
+
+            try
+            {
+                handler.Handle(new ProductInStockUpdateCommand()
+                {
+                    Items = new List<ProductInStockUpdateItem>()
+                {
+                    new ProductInStockUpdateItem() {
+                        ProductId = productId,
+                        Action = common.Enums.ProductInStockAction.Substract,
+                        Stock = 2
+                    }
+                }
+                }, new CancellationToken()).Wait();
+            } catch (AggregateException ae)
+            {
+                var exception = ae.GetBaseException();
+                Assert.IsTrue(exception is ProductInStockUpdateCommandException);
+                throw exception;
+            }
         }
     }
 }
