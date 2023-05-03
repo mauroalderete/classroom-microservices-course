17a69d271220588eb68e5c4f4c0d8662320d4346
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-03 12:56:09 -0300

    feat: create test proyect to product in stock update event

---
+++ KodotiCommerce/src/Services/Catalog/Catalog.Test/ProductInStockUpdateEventHandler.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catalog.Test
+{
+    [TestClass]
+    public class ProductInStockUpdateEventHandler
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
