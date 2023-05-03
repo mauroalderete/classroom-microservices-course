730050eb93c20ba6374ea348c4ded1910a2447dc
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-03 15:48:15 -0300

    feat: add client queries services and register them

--- KodotiCommerce/src/Services/Customer/Customer.Api/Startup.cs
+++ KodotiCommerce/src/Services/Customer/Customer.Api/Startup.cs
@@ -1,6 +1,9 @@
+using Customer.Persistence.Database;
+using Customer.Service.Queries;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,14 @@ namespace Customer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(opt =>
+                opt.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection"),
+                    sqlOptions => sqlOptions.MigrationsHistoryTable("__EFMigrationsHistory", "Customer"))
+            );
+
+            services.AddTransient<IClientQueryService, ClientQueryService>();
+
             services.AddControllers();
         }
 
---
+++ KodotiCommerce/src/Services/Customer/Customer.Service.Queries/DTO/ClientDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer.Service.Queries.DTO
+{
+    public class ClientDto
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+    }
+}
