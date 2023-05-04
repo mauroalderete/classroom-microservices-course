88e01490656a8eef81b80e96230289a31a5d53bc
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-04 16:20:27 -0300

    feat: implement identity database

---
+++ KodotiCommerce/src/Services/Identity/Identity.Persistence.Database/Configuration/ApplicationRoleConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Identity.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Persistence.Database.Configuration
+{
+    public class ApplicationRoleConfiguration
+    {
+        public ApplicationRoleConfiguration(EntityTypeBuilder<ApplicationRole> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(r => r.Id);
+            const string user = "Admin";
+            entityTypeBuilder.HasData(new ApplicationRole
+            {
+                Id = Guid.NewGuid().ToString().ToLower(),
+                Name = user,
+                NormalizedName = user.ToUpper()
+            });
+
+            entityTypeBuilder.HasMany(r => r.UserRoles)
+                .WithOne(ur => ur.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+        }
+    }
+}
