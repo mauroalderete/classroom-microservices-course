8659015a803986a14a5e8dbf3021a02d26a8736b
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-04 14:10:31 -0300

    feat: extend catalog proxy to send stock update request to a queue instead of make a direct http request

---
+++ KodotiCommerce/src/Services/Order/Order.Service.Proxies/AzureServiceBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Service.Proxies
+{
+    public class AzureServiceBus
+    {
+        public string ConnectionString { get; set; }
+        public string QueueName { get; set; }
+    }
+}
---
+++ KodotiCommerce/src/Services/Order/Order.Service.Proxies/Catalog/CatalogQueueProxy.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Options;
+using Order.Service.Proxies.Catalog.Commands;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Order.Service.Proxies.Catalog
+{
+    public class CatalogQueueProxy : ICatalogProxy
+    {
+        private readonly AzureServiceBus _azure;
+
+        public CatalogQueueProxy(IOptions<AzureServiceBus> azure)
+        {
+            _azure = azure.Value;
+        }
+        public async Task UpdateStockAsync(ProductInStockUpdateCommand command)
+        {
+            var queueClient = new QueueClient(_azure.ConnectionString, _azure.QueueName);
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(command), encoding: System.Text.Encoding.UTF8, "application/json");
+
+            await queueClient.SendAsync(new Message(content.ReadAsByteArrayAsync().Result));
+
+            await queueClient.CloseAsync();
+        }
+    }
+}
