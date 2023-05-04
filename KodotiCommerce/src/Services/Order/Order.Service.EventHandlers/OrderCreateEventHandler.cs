74b8634cbd8b23b3e8b2ac89a2b917b4f8e3c983
Mauro Alderete <alderete.mauro@gmail.com>
2023-05-04 10:36:33 -0300

    feat: separate steps of the new order and use a transaction

--- KodotiCommerce/src/Services/Order/Order.Service.EventHandlers/OrderCreateEventHandler.cs
+++ KodotiCommerce/src/Services/Order/Order.Service.EventHandlers/OrderCreateEventHandler.cs
@@ -28,31 +28,60 @@ namespace Order.Service.EventHandlers
         public async Task Handle(OrderCreateCommand notification, CancellationToken cancellationToken)
         {
             _logger.LogInformation("OrderCreateCommand started");
-            var order = new Domain.Order()
-            {
-                ClientId = notification.ClientId,
-                CreatedAt = DateTime.Now,
-                Status = notification.Status,
-                PaymentType = notification.PaymentType,
-            };
+            //var order = new Domain.Order()
+            //{
+            //    ClientId = notification.ClientId,
+            //    CreatedAt = DateTime.Now,
+            //    Status = notification.Status,
+            //    PaymentType = notification.PaymentType,
+            //};
 
-            foreach( var item in notification.Items)
+            var order = new Domain.Order();
+
+            using (var transaction = await _dbContext.Database.BeginTransactionAsync())
             {
-                order.Items.Add(new OrderDetail()
-                {
-                    ProductId = item.ProductId,
-                    Quantity = item.Quantity,
-                    UnitPrice = item.UnitPrice,
-                    Total = item.Quantity * item.UnitPrice
-                });
+                _logger.LogInformation("Preparing order detail");
+                PrepareOrderDetail(order, notification);
+
+                _logger.LogInformation("Preparing order header");
+                PrepareOrder(order, notification);
+
+                _logger.LogInformation("Saving order");
+                await _dbContext.AddAsync(order);
+                await _dbContext.SaveChangesAsync();
+                _logger.LogInformation($"Order ${order.OrderId} was saved");
+
+                //TODO: Update Stock notification
+                _logger.LogInformation("Updating stock");
+
+                await transaction.CommitAsync();
             }
-
-            order.Total = order.Items.Sum(x => x.Total);
 
             await _dbContext.AddAsync(order);
 
             await _dbContext.SaveChangesAsync();
             _logger.LogInformation("OrderCreateCommand completed");
+        }
+
+        private void PrepareOrderDetail(Domain.Order order, OrderCreateCommand notification)
+        {
+            order.Items = notification.Items.Select(x => new OrderDetail()
+            {
+                ProductId = x.ProductId,
+                Quantity = x.Quantity,
+                UnitPrice = x.UnitPrice,
+                Total = x.Quantity * x.UnitPrice
+            }).ToList();
+        }
+
+        private void PrepareOrder(Domain.Order order, OrderCreateCommand notification)
+        {
+            order.ClientId = notification.ClientId;
+            order.CreatedAt = DateTime.Now;
+            order.Status = notification.Status;
+            order.PaymentType = notification.PaymentType;
+
+            order.Total = order.Items.Sum(x => x.Total);
         }
     }
 }
